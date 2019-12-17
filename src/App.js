c94ee857bbe5529b94fdd2d9e010c6857da0df9c
Barbonov Artem <abarbonov@gmail.com>
2019-12-17 11:46:36 +0600

    completed project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Layout from "./hoc/Layout/Layout";
+import Home from "./containers/Home/Home";
+import Portfolio from "./containers/Portfolio/Portfolio";
+import Contacts from "./containers/Contacts/Contacts";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	return (
+		<Layout>
+			<Switch>
+				<Route path='/contacts' component={Contacts}></Route>
+				<Route path='/portfolio' component={Portfolio}></Route>
+				<Route path='/' component={Home}></Route>
+			</Switch>
+		</Layout>
+	);
 }
-
-export default App;
